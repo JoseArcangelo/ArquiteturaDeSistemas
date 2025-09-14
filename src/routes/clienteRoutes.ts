15ea81a80123e523df3d8c4b4b6673f15896b0c7
José Arcangelo de Souza Garlet <127755871+JoseArcangelo@users.noreply.github.com>
2025-09-14 20:28:23 -0300

    Delete src directory

--- src/routes/clienteRoutes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import * as clienteController from "../controllers/clienteController";
-
-const router = Router();
-
-router.post("/", clienteController.criarCliente); 
-router.get("/", clienteController.listarClientes); 
-
-export default router;
--- src/server.ts
+++
@@ -1,7 +0,0 @@
-import app from "./index";
-
-const PORT = 4000;
-
-app.listen(PORT, () => {
-  console.log(`🚀 Server running at http://localhost:${PORT}`);
-});
